64911b2385fac7020ed4110b325755f494faa8ff
K.D. Hubbard <hubbard.kd@gmail.com>
2023-06-12 10:32:57 -0600

    WiP remove button

--- app/controllers/passenger_flights_controller.rb
+++ app/controllers/passenger_flights_controller.rb
@@ -1,9 +1,15 @@
 class PassengerFlightsController < ApplicationController
-  def delete 
-    require 'pry'; binding.pry
-  end
 
   def destroy 
-    require 'pry'; binding.pry
+    passenger_flight = PassengerFlight.find_by
+      (passenger_id: params[:passenger_id, flight_id: params[:flight_id]])
+    passenger_flight.destroy
+    redirect_to flights_path
+  end
+
+  private
+
+  def p_flight_params
+    params.permit(:passenger_id, :flight_id)
   end
 end
