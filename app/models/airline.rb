127a40b208223444e0113c0fa0616aad974e01e4
Ashley Plasket <ashley.plasket@gmail.com>
2023-06-12 12:39:50 -0400

    update association and add AR method

--- app/models/airline.rb
+++ app/models/airline.rb
@@ -1,3 +1,8 @@
 class Airline < ApplicationRecord
    has_many :flights
+   has_many :passengers, through: :flights
+
+   def adult_passengers_distinct
+      passengers.where("passengers.age >= 18").distinct
+   end
 end
