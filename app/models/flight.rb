ed5acca25c39042527ab278d67b2d6026c85d1e4
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-06-12 12:02:54 -0600

    remaninder

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -2,4 +2,8 @@ class Flight < ApplicationRecord
    belongs_to :airline
    has_many :passengers_flights
    has_many :passengers, through: :passengers_flights
+
+   def remove_passenger(passenger)
+      passengers.delete(passenger)
+   end
 end
