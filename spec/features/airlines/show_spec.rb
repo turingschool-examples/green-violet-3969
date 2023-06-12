e3c80c77e87c2934d2e565d1a33b228ea426fb4f
Steven Stackhouse <stackmm@gmail.com>
2023-06-12 10:40:49 -0600

    completes airlines view and user story 3

--- spec/features/airlines/show_spec.rb
+++ spec/features/airlines/show_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe "Airline's Show Page" do
     @flight_1 = @airline_1.flights.create!(number: "123", date: "01/01/23", departure_city: "Denver", arrival_city: "Las Vegas")
     @flight_2 = @airline_1.flights.create!(number: "456", date: "01/05/23", departure_city: "Denver", arrival_city: "St Louis")
     @flight_3 = @airline_2.flights.create!(number: "789", date: "01/02/23", departure_city: "Chicago", arrival_city: "New York")
+    @flight_4 = @airline_2.flights.create!(number: "101", date: "01/02/23", departure_city: "Reno", arrival_city: "New York")
 
     @passenger_1 = Passenger.create!(name: "Michael", age: 9) 
     @passenger_2 = Passenger.create!(name: "Meg", age: 17)
@@ -17,17 +18,19 @@ RSpec.describe "Airline's Show Page" do
     @passenger_6 = Passenger.create!(name: "Bob", age: 18)
     @passenger_7 = Passenger.create!(name: "Sue", age: 42)
 
-    @fp_1 = FlightPassenger.create!(flight: @flight_1, passenger: @passenger_1) 
-    @fp_2 = FlightPassenger.create!(flight: @flight_1, passenger: @passenger_2)
+    @fp_1 = FlightPassenger.create!(flight: @flight_1, passenger: @passenger_1) # minor
+    @fp_2 = FlightPassenger.create!(flight: @flight_1, passenger: @passenger_2) # minor
     @fp_3 = FlightPassenger.create!(flight: @flight_1, passenger: @passenger_3)
     @fp_9 = FlightPassenger.create!(flight: @flight_1, passenger: @passenger_6)
-
     @fp_4 = FlightPassenger.create!(flight: @flight_2, passenger: @passenger_4)
-    @fp_5 = FlightPassenger.create!(flight: @flight_2, passenger: @passenger_1) 
+    @fp_5 = FlightPassenger.create!(flight: @flight_2, passenger: @passenger_1) # minor
     @fp_6 = FlightPassenger.create!(flight: @flight_2, passenger: @passenger_5)
-    @fp_7 = FlightPassenger.create!(flight: @flight_2, passenger: @passenger_6)
+    @fp_7 = FlightPassenger.create!(flight: @flight_2, passenger: @passenger_6) # duplicate
 
     @fp_8 = FlightPassenger.create!(flight: @flight_3, passenger: @passenger_7)
+    @fp_10 = FlightPassenger.create!(flight: @flight_4, passenger: @passenger_7) # duplicate
+    @fp_11 = FlightPassenger.create!(flight: @flight_4, passenger: @passenger_2) # minor
+    @fp_12 = FlightPassenger.create!(flight: @flight_4, passenger: @passenger_6) 
   end
 
   it "displays a list of unique adult (age 18+) passengers that have flights on that airline" do
@@ -40,5 +43,11 @@ RSpec.describe "Airline's Show Page" do
     expect(page).to_not have_content(@passenger_1.name) # minor
     expect(page).to_not have_content(@passenger_2.name) # minor
     expect(page).to_not have_content(@passenger_7.name) # different airline
+
+    visit airline_path(@airline_2)
+
+    expect(page).to have_content(@passenger_6.name)
+    expect(page).to have_content(@passenger_7.name, count: 1) # on two flights (only appear once on list)
+    expect(page).to_not have_content(@passenger_2.name) # minor
   end
 end
