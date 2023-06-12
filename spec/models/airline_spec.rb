76e8e759b53d7a8e835c3c72a326743668bc8487
Andy Weissman <andyweissman6@gmail.com>
2023-06-12 09:19:02 -0600

    setup complete

--- spec/models/airline_spec.rb
+++ spec/models/airline_spec.rb
@@ -3,5 +3,6 @@ require "rails_helper"
 RSpec.describe Airline, type: :model do
   describe "relationships" do
     it {should have_many :flights}
+    it {should have_many(:passengers).through(:flights)}
   end
 end
