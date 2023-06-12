c9ddd93fe7f36cdf718209ffadb2d63cb16de856
K.D. Hubbard <hubbard.kd@gmail.com>
2023-06-12 10:49:32 -0600

    add passengers association

--- app/models/airline.rb
+++ app/models/airline.rb
@@ -1,3 +1,5 @@
 class Airline < ApplicationRecord
    has_many :flights
+   has_many :passenger_flights, through: :flights
+   has_many :passengers, through: :passenger_flights
 end
--- spec/models/airline_spec.rb
+++ spec/models/airline_spec.rb
@@ -3,5 +3,7 @@ require "rails_helper"
 RSpec.describe Airline, type: :model do
   describe "relationships" do
     it {should have_many :flights}
+    it {should have_many(:passenger_flights).through(:flights)}
+    it {should have_many(:passengers).through(:passenger_flights)}
   end
 end
