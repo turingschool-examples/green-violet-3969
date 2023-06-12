433af403e61186e93ae2220fe14ae2ad490acfb6
K.D. Hubbard <hubbard.kd@gmail.com>
2023-06-12 09:23:01 -0600

    add migrations and models for passenger and associations

--- spec/models/flight_spec.rb
+++ spec/models/flight_spec.rb
@@ -3,5 +3,7 @@ require "rails_helper"
 RSpec.describe Flight, type: :model do
   describe "relationships" do
     it {should belong_to :airline}
+    it {should have_many :passenger_flights}
+    it {should have_many(:passengers).through(:passenger_flights)}
   end
 end
