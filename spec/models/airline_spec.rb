a9c256a97abbd53231f7919f59d007096b902a81
Garrett Gregor <garrett.gregor@gmail.com>
2023-06-12 09:22:47 -0600

    Test: associations

--- spec/models/airline_spec.rb
+++ spec/models/airline_spec.rb
@@ -2,6 +2,8 @@ require "rails_helper"
 
 RSpec.describe Airline, type: :model do
   describe "relationships" do
-    it {should have_many :flights}
+    it { should have_many :flights }
+    it { should have_many(:flight_passengers).through(:flights) }
+    it { should have_many(:passengers).through(:flight_passengers) }
   end
 end
--- spec/models/flight_spec.rb
+++ spec/models/flight_spec.rb
@@ -2,6 +2,8 @@ require "rails_helper"
 
 RSpec.describe Flight, type: :model do
   describe "relationships" do
-    it {should belong_to :airline}
+    it { should belong_to :airline }
+    it { should have_many(:flight_passengers) }
+    it { should have_many(:passengers).through(:flight_passengers) }
   end
 end
