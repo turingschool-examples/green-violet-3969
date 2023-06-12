5e12d488764716ccb2d3e3a03546e324d43ff4c3
Jimmy Jurado <jurado140@gmail.com>
2023-06-12 13:07:14 -0400

    Adds unique adult passenger model method to airline. Missing model testing.

--- app/models/airline.rb
+++ app/models/airline.rb
@@ -1,4 +1,8 @@
 class Airline < ApplicationRecord
    has_many :flights
    has_many :passengers, through: :flights
+
+   def unique_adult_passengers
+      passengers.where("passengers.age >= 18").distinct
+   end
 end
