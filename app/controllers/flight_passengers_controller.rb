cea93477065a28e9915c9665e0321f4f6fdf7d87
Nick Sacco <nicksacco10@gmail.com>
2023-06-12 09:43:15 -0700

    feat: nested resource routes going to flight_passenger controller

---
+++ app/controllers/flight_passengers_controller.rb
@@ -0,0 +1,5 @@
+class FlightPassengersController < ApplicationController
+  def destroy
+    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  resources :flights, only: [:index]
+  resources :flights, only: [:index] do
+    resources :passengers, controller: 'flight_passengers', only: [:destroy]
+  end
 end
