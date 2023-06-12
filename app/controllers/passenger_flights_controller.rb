62ef0659038ccf6c07c7cd98b6245d52fe41a7ba
K.D. Hubbard <hubbard.kd@gmail.com>
2023-06-12 10:35:00 -0600

    add remove button to index

--- app/controllers/passenger_flights_controller.rb
+++ app/controllers/passenger_flights_controller.rb
@@ -1,8 +1,7 @@
 class PassengerFlightsController < ApplicationController
 
   def destroy 
-    passenger_flight = PassengerFlight.find_by
-      (passenger_id: params[:passenger_id, flight_id: params[:flight_id]])
+    passenger_flight = PassengerFlight.find_by(passenger_id: params[:passenger_id], flight_id: params[:flight_id])
     passenger_flight.destroy
     redirect_to flights_path
   end
