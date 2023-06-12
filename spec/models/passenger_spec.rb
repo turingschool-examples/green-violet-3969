e376e926047cb0ec54f83b2839bdadb33ad3df89
Kim Bergstrom <kimbergstrom78@gmail.com>
2023-06-12 09:25:07 -0600

    Add relationships

--- spec/models/passenger_spec.rb
+++ spec/models/passenger_spec.rb
@@ -3,5 +3,7 @@ require "rails_helper"
 RSpec.describe Passenger, type: :model do
   describe "relationships" do
     it { should have_many :passenger_flights }
+    it { should have_many(:flights).through(:passenger_flights) }
+
   end
 end
