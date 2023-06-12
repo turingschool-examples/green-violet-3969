e8b9cf59be8b244e5d0f4a22164e152f1ea79cb5
Nick Sacco <nicksacco10@gmail.com>
2023-06-12 09:33:59 -0700

    test: remove passenger from flight tests and added ending div tag to view

--- spec/features/flights/index_spec.rb
+++ spec/features/flights/index_spec.rb
@@ -15,6 +15,23 @@ RSpec.describe "Flights Index Page", type: :feature do
           expect(page).to have_content("Passengers:")
           expect(page).to have_content(@pass1.name)
           expect(page).to have_content(@pass2.name)
+          expect(page).to_not have_content(@pass3.name)
+        end
+      end
+    end
+    
+    describe "Removing a passenger from a flight" do
+      it "has a link to remove a passenger from a specific flight" do
+        visit(flights_path)
+        within "#flight_info_#{@flight1.id}" do
+          expect(page).to have_link("Remove #{@pass1.name}")
+          click_link("Remove #{@pass1.name}")
+          expect(current_path).to eq(flights_path)
+          expect(page).to_not have_content(@pass1.name)
+        end
+        within "#flight_info_#{@flight2.id}" do
+          expect(page).to have_content(@pass1.name)
+          expect(page).to have_link("Remove #{@pass1.name}")
         end
       end
     end
