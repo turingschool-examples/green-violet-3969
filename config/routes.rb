ae474ae2eef7ae2dc566da8e720ae9824b8453fb
Ashley Plasket <ashley.plasket@gmail.com>
2023-06-12 12:22:48 -0400

    add airlines show route

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
 
   get "/flights", to: "flights#index"
   delete "/flights/:flight_id/passengers/:passenger_id", to: "flight_passengers#destroy"
+
+  get "/airlines/:id", to: "airlines#show"
 end
