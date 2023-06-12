2c4a9b70d7db4a1fe322f36a1331f5af4fc74199
Lauren Golden <llgolden@gmail.com>
2023-06-12 11:31:08 -0600

    extension setup

--- spec/features/airline/show_spec.rb
+++ spec/features/airline/show_spec.rb
@@ -9,6 +9,10 @@ RSpec.describe "the airlines show page" do
     @flight3 = @airline1.flights.create!(number: "6493", date: "08/02/23", departure_city: "Reno", arrival_city: "Denver")
     @flight4 = @airline1.flights.create!(number: "3476", date: "07/01/23", departure_city: "Seattle", arrival_city: "Denver")
     @flight5 = @airline2.flights.create!(number: "2031", date: "08/03/23", departure_city: "Denver", arrival_city: "Portland")
+    @flight6 = @airline1.flights.create!(number: "8452", date: "08/03/23", departure_city: "Denver", arrival_city: "Miami")
+    @flight7 = @airline1.flights.create!(number: "9876", date: "09/03/23", departure_city: "Miami", arrival_city: "Portland")
+    @flight8 = @airline1.flights.create!(number: "1234", date: "10/03/23", departure_city: "Seattle", arrival_city: "Portland")
+    
     @passenger1 = @flight1.passengers.create!(name: "Jolene", age: "18")
     @passenger2 = @flight1.passengers.create!(name: "Simon", age: "3")
     @passenger3 = @flight1.passengers.create!(name: "Brett", age: "38")
@@ -20,6 +24,9 @@ RSpec.describe "the airlines show page" do
     @passenger9 = @flight5.passengers.create!(name: "Teresa", age: "65")
     @passenger10 = @flight4.passengers.create!(name: "Eric", age: "22")
     FlightPassenger.create!(flight: @flight1, passenger: @passenger10)
+    FlightPassenger.create!(flight: @flight6, passenger: @passenger3)
+    FlightPassenger.create!(flight: @flight7, passenger: @passenger3)
+    FlightPassenger.create!(flight: @flight8, passenger: @passenger7)
 
     visit "/airlines/#{@airline1.id}"
   end
@@ -36,5 +43,14 @@ RSpec.describe "the airlines show page" do
     expect(page).to_not have_content("#{@passenger6.name}")
     expect(page).to_not have_content("#{@passenger2.name}")
     expect(page).to_not have_content("#{@passenger9.name}")
+  end
+# Extension, Frequent Flyers
+  it "sorts the unique list of adult passengers by number of flights" do 
+    expect(@passenger3.name).to appear_before(@passenger7.name)
+    expect(@passenger7.name).to appear_before(@passenger1.name)
+    expect(@passenger7.name).to appear_before(@passenger4.name)
+    expect(@passenger7.name).to appear_before(@passenger5.name)
+    expect(@passenger7.name).to appear_before(@passenger8.name)
+    expect(@passenger7.name).to appear_before(@passenger10.name)
   end
 end
