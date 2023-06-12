a2657e950d1d9311af053b59254bf1353a521c4c
Kim Bergstrom <kimbergstrom78@gmail.com>
2023-06-12 10:16:18 -0600

    Add passenger#destroy from flights index

--- spec/features/flights/index_spec.rb
+++ spec/features/flights/index_spec.rb
@@ -17,19 +17,13 @@ RSpec.describe "flight#index", type: :feature do
     PassengerFlight.create!(flight: @flight1, passenger: @passenger2)
     PassengerFlight.create!(flight: @flight1, passenger: @passenger3)
     PassengerFlight.create!(flight: @flight1, passenger: @passenger4)
+    PassengerFlight.create!(flight: @flight2, passenger: @passenger1)
+    PassengerFlight.create!(flight: @flight2, passenger: @passenger2)
   end
-
-  # User Story 1, Flights Index Page
-
-# As a visitor
-# When I visit the flights index page
-# I see a list of all flight numbers
-# And next to each flight number I see the name of the Airline of that flight
-# And under each flight number I see the names of all that flight's passengers
 
   it "should display all flights" do
     visit "/flights"
-    save_and_open_page
+    # save_and_open_page
 
     expect(page).to have_content(@flight1.number)
     expect(page).to have_content(@flight2.number)
@@ -40,6 +34,40 @@ RSpec.describe "flight#index", type: :feature do
       expect(page).to have_content(@passenger2.name)
       expect(page).to have_content(@passenger3.name)
       expect(page).to have_content(@passenger4.name)
+    end
+  end
+
+  # User Story 2, Remove a Passenger from a Flight
+
+  # As a visitor
+  # When I visit the flights index page
+  # Next to each passengers name
+  # I see a link or button to remove that passenger from that flight
+  # When I click on that link/button
+  # I'm returned to the flights index page
+  # And I no longer see that passenger listed under that flight,
+  # And I still see the passenger listed under the other flights they were assigned to.
+
+  it "can remove a passenger from the flight" do
+    visit "/flights"
+    save_and_open_page
+    within("#passenger-list-#{@flight1.id}") do
+      expect(page).to have_content(@passenger1.name)
+      expect(page).to have_content(@passenger2.name)
+
+    click_on "Remove #{@passenger1.name}"
+    end
+
+  expect(current_path).to eq("/flights")
+
+    within("#passenger-list-#{@flight1.id}") do
+      expect(page).not_to have_content(@passenger1.name)
+      expect(page).to have_content(@passenger2.name)
+    end
+
+    within("#passenger-list-#{@flight2.id}") do
+      expect(page).to have_content(@passenger1.name)
+      expect(page).to have_content(@passenger2.name)
     end
   end
 end
