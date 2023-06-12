4fc0929029249520358acd9811138580d1051a55
Andrew Stilmock <andrew.stilmock@gmail.com>
2023-06-12 09:17:56 -0600

    Test: Model association test setup

--- spec/models/flight_spec.rb
+++ spec/models/flight_spec.rb
@@ -3,5 +3,7 @@ require "rails_helper"
 RSpec.describe Flight, type: :model do
   describe "relationships" do
     it {should belong_to :airline}
+    it {should have_many :bookings}
+    it {should have_many(:passengers).through(:bookings)}
   end
 end
