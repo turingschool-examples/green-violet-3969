229875659cd70af3b961485b712c9e6143fe8367
Steven Stackhouse <stackmm@gmail.com>
2023-06-12 10:32:21 -0600

    add unique_adult_passengers method

--- app/models/airline.rb
+++ app/models/airline.rb
@@ -3,4 +3,8 @@ class Airline < ApplicationRecord
    has_many :passengers, through: :flights
 
    validates_presence_of :name
+
+   def unique_adult_passengers
+     passengers.where("passengers.age >= 18").distinct
+   end
 end
