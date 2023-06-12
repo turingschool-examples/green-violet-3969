465fb1d8f45bdb3ff14fe1f0d364b7586c635f24
K.D. Hubbard <hubbard.kd@gmail.com>
2023-06-12 11:25:12 -0600

    add frequent_flyers method

--- app/models/airline.rb
+++ app/models/airline.rb
@@ -3,7 +3,17 @@ class Airline < ApplicationRecord
    has_many :passenger_flights, through: :flights
    has_many :passengers, through: :passenger_flights
 
-   def passenger_names
-      passengers.where('passengers.age >= 18').distinct.pluck(:name)
+   # def passenger_names
+   #    passengers.where('passengers.age >= 18').distinct.pluck(:name)
+   # end
+
+   def frequent_flyers
+      passengers.joins(:passenger_flights)
+      .select('passengers.id, 
+         passengers.name, 
+         count(passenger_flights.id) as flights')
+      .group('passengers.id')
+      .where('passengers.age >=18')
+      .order('flights desc')
    end
 end
