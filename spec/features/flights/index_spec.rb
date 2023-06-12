c08e1219d4b5d1431f1e8d1dc69a7bf4ad9f8b06
Nick Sacco <nicksacco10@gmail.com>
2023-06-12 09:22:31 -0700

    fix tests for us1

--- spec/features/flights/index_spec.rb
+++ spec/features/flights/index_spec.rb
@@ -11,11 +11,10 @@ RSpec.describe "Flights Index Page", type: :feature do
         visit(flights_path)
         within "#flight_info_#{@flight2.id}" do
           expect(page).to have_content("Flight Number: #{@flight2.number}")
-          expect(page).to have_content("Airline: #{@flight2.airline}")
+          expect(page).to have_content("Airline: #{@flight2.airline.name}")
           expect(page).to have_content("Passengers:")
           expect(page).to have_content(@pass1.name)
           expect(page).to have_content(@pass2.name)
-          expect(page).to_not have_content(@pass3.name)
         end
       end
     end
