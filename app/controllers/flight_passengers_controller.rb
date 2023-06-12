96969ce9fd191a06132cdcc1549dee28eda2f2a0
Nick Sacco <nicksacco10@gmail.com>
2023-06-12 09:48:05 -0700

    complete us2

--- app/controllers/flight_passengers_controller.rb
+++ app/controllers/flight_passengers_controller.rb
@@ -1,5 +1,8 @@
 class FlightPassengersController < ApplicationController
   def destroy
-    
+    @flight = Flight.find(params[:flight_id])
+    @passenger = Passenger.find(params[:id])
+    @flight.passengers.destroy(@passenger)
+    redirect_to(flights_path)
   end
 end
